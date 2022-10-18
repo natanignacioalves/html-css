8966223119185a27132d5f826f4c0ca9ea3720aa
Natan Alves <108841058+natanignacioalves@users.noreply.github.com>
2022-10-18 19:47:53 -0300

    Initial commit

---
+++ exercícios/Exerc livro nodejs/1index.js
@@ -0,0 +1,22 @@
+//Alguns operadores apenas pra praticar..
+let a = 5
+let b =2
+/*
+console.log('a=' + a)
+console.log('b=' +b )
+console.log('a%b=' + (a%b))
+console.log('a++' + (a++))//soma mas só imprime o resultado se chmar na próxima linha
+console.log('b++' + (b++))
+console.log('++a' + (++a)) //soma e já imprime o resultado direto (adicionando mais um)
+console.log('++b' + (++b))
+*/
+
+//----------------------------------------------------------------------------------------
+
+//Laços de repetição
+
+
+
+for (let i = 0; i < 1e4; i++) {
+    console.log(i)
+}
